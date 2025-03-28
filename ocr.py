d7f5a21c9c1b685cac64c9d51717b1a0c44d5905
rhy3h <rhy3htw@gmail.com>
2025-03-28 18:19:34 +0800

    feat: update save test info into ocr module

--- ocr.py
+++ ocr.py
@@ -1,8 +1,69 @@
+import os
+import csv
+from typing import Dict
+
 import easyocr
 
+row_label = ['檔案名稱', '正確答案', '預測答案']
+
+class PredictInfo:
+    def __init__(self, file: str, ans: str, result: str):
+        self.file = file
+        self.ans = ans
+        self.result = result
+
+    def to_dict(self):
+        return {row_label[0]: self.file, row_label[1]: self.ans, row_label[2]: self.result}
+
 class Ocr:
-    def __init__(self):
+    def __init__(self, data_path: str, result_path: str):
+        self.data_path = data_path
+        self.result_path = result_path
+
         self.easyocr_reader = easyocr.Reader(['en'])
 
-    def predictByEasyOcr(self, file_path: str):
-        return self.easyocr_reader.readtext(file_path, detail = 0)
+        os.makedirs(result_path, exist_ok=True)
+
+    def predict(self, predictInfo: Dict[str, PredictInfo], model: str):
+        data = []
+
+        correct = 0
+        cnt = 0
+
+        for key in predictInfo:
+            file_path = f'{self.data_path}/{file_info.file}'
+
+            file_info = predictInfo[key]
+            prediect_ans = ''
+
+            if file_info.result == '':
+                if model == 'easyocr':
+                    result = self.easyocr_reader.readtext(file_path, detail = 0)
+                    prediect_ans = result[0].lower()
+                else:
+                    raise Exception('Not support model')
+
+            file_info.result = prediect_ans
+
+            data.append(file_info.to_dict())
+
+            if file_info.result == file_info.ans:
+                correct += 1
+            cnt += 1
+
+            status = f'Precision: {correct / cnt:.2f} ({correct} / {cnt})'
+            print(status, end='\r')
+
+            txt_filename = f'{self.result_path}/{model}.txt'
+            with open(txt_filename, mode='w', newline='', encoding='utf-8') as file:
+                file.write(status)
+
+            csv_filename = f'{self.result_path}/{model}.csv'
+            with open(csv_filename, mode='w', newline='', encoding='utf-8') as file:
+                writer = csv.DictWriter(file, fieldnames=row_label)
+
+                writer.writeheader()
+                writer.writerows(data)
+
+            result = self.easyocr_reader.readtext(file_path, detail = 0)
+            file_info.result = result[0].lower()
--- tixcraft.py
+++ tixcraft.py
@@ -9,7 +9,7 @@ from typing import Dict
 import requests
 from pathlib import Path
 
-from ocr import Ocr
+from ocr import Ocr, PredictInfo
 
 CACA_QUEST_URL = 'https://gen.caca01.com/ttcode/quest'
 CACA_TEST_URL = 'https://gen.caca01.com/rd/test'
@@ -47,16 +47,6 @@ def collect(loop: int = 100):
         getQuestData()
         getTestData()
 
-row_label = ['檔案名稱', '正確答案', '預測答案']
-
-class PredictInfo:
-    def __init__(self, file: str, ans: str, result: str):
-        self.file = file
-        self.ans = ans
-        self.result = result
-
-    def to_dict(self):
-        return {row_label[0]: self.file, row_label[1]: self.ans, row_label[2]: self.result}
 
 def getMap(files: list[str]) -> Dict[str, PredictInfo]:
     data = {}
@@ -70,48 +60,14 @@ def getMap(files: list[str]) -> Dict[str, PredictInfo]:
     return data
 
 def test():
-    os.makedirs(TEST_PATH, exist_ok=True)
-
-    ocr = Ocr()
+    ocr = Ocr(ROOT_PATH, TEST_PATH)
 
     folder_path = Path(ROOT_PATH)
     files = [f.name for f in folder_path.iterdir() if f.is_file()]
 
-    correct = 0
-    cnt = 0
-
-    data = []
-
     file_map = getMap(files)
 
-    for key in file_map:
-        file_info = file_map[key]
-
-        file_path = f'{ROOT_PATH}/{file_info.file}'
-
-        if file_info.result == '':
-            result = ocr.predictByEasyOcr(file_path)
-            file_info.result = result[0].lower()
-
-        data.append(file_info.to_dict())
-
-        if file_info.result == file_info.ans:
-            correct += 1
-        cnt += 1
-
-        status = f'Precision: {correct / cnt:.2f} ({correct} / {cnt})'
-        print(status, end='\r')
-
-        txt_filename = f'{TEST_PATH}/easyocr.txt'
-        with open(txt_filename, mode='w', newline='', encoding='utf-8') as file:
-            file.write(status)
-
-        csv_filename = f'{TEST_PATH}/easyocr.csv'
-        with open(csv_filename, mode='w', newline='', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=row_label)
-
-            writer.writeheader()
-            writer.writerows(data)
+    ocr.predict(file_map, 'easyocr')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
